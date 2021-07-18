28511b4569ed9fd9bc1db9b02180fbafb7d4225d
Dmitry Karas <dmitrythekaras@gmail.com>
2021-07-18 10:22:21 +0300

    added SearchForm

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,10 @@
+import { Form, Input, Button } from './SearchForm.styled'
+
+export const SearchForm = () => {
+  return (
+    <Form>
+      <Input />
+      <Button type="submit">Search</Button>
+    </Form>
+  )
+}
---
+++ src/components/SearchForm/SearchForm.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled/macro'
+
+export const Form = styled.form``
+
+export const Input = styled.input``
+
+export const Button = styled.button``
